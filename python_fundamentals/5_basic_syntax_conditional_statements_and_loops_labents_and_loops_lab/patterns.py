abf8fde1225043ffb51e1ba54bad3477f271bb55
Ivan Tsvetanov <102186881+1van101@users.noreply.github.com>
2022-05-25 21:05:45 +0300

    Update patterns.py

--- python_fundamentals/5_basic_syntax_conditional_statements_and_loops_labents_and_loops_lab/patterns.py
+++ python_fundamentals/5_basic_syntax_conditional_statements_and_loops_labents_and_loops_lab/patterns.py
@@ -1,1 +1,9 @@
-
+number = int(input())
+for r in range(1, number + 1):
+    for c in range(1, r + 1):
+        print("*", end="")
+    print()
+for r in range(number - 1, 0, -1):
+    for c in range(r, 0, -1):
+        print("*", end="")
+    print()
