95318dc9d1d456f9b9d03cd514ebc003f7fe6de5
1.tsvetanov <ivan.iv.cv@gmail.com>
2022-06-06 22:28:54 +0300

    update

--- python_fundamentals/09_lists_advanced_lab/even_numbers.py
+++ python_fundamentals/09_lists_advanced_lab/even_numbers.py
@@ -1,9 +1,14 @@
-str_with_nums = input().split(", ")
+numbers = [int(x) for x in input().split(", ")]
+result = [(x[0]) for x in enumerate(numbers) if x[1] % 2 == 0]
+print(result)
 
-list_with_indexes = []
+#======================================
+# str_with_nums = input().split(", ")
 
-for i, n in enumerate(str_with_nums):
-    n = int(n)
-    if n % 2 == 0:
-        list_with_indexes.append(i)
-print(list_with_indexes)
+# list_with_indexes = []
+
+# for i, n in enumerate(str_with_nums):
+#     n = int(n)
+#     if n % 2 == 0:
+#         list_with_indexes.append(i)
+# print(list_with_indexes)
--- python_fundamentals/09_lists_advanced_lab/palindrome_strings.py
+++ python_fundamentals/09_lists_advanced_lab/palindrome_strings.py
@@ -1,12 +1,22 @@
 words_list = input().split()
 palindrome_input = input()
 
-list_with_palindromes = []
-for word in words_list:
-    if word == word[::-1]:
-        list_with_palindromes.append(word)
-
+list_with_palindromes = [x for x in words_list if x[::-1] == x]
 number = list_with_palindromes.count(palindrome_input)
 
 print(list_with_palindromes)
 print(f"Found palindrome {number} times")
+
+#==================================================================
+# words_list = input().split()
+# palindrome_input = input()
+
+# list_with_palindromes = []
+# for word in words_list:
+#     if word == word[::-1]:
+#         list_with_palindromes.append(word)
+
+# number = list_with_palindromes.count(palindrome_input)
+
+# print(list_with_palindromes)
+# print(f"Found palindrome {number} times")
