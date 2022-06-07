590831356d301761692037368e8fdd147ee17afc
1.tsvetanov <ivan.iv.cv@gmail.com>
2022-06-07 13:15:24 +0300

    updates

--- python_fundamentals/08_exercise_functions/add_and_subtract.py
+++ python_fundamentals/08_exercise_functions/add_and_subtract.py
@@ -6,14 +6,13 @@ def subtract(num3, result):
     return result - num3
 
 
-def add_and_subtract():
-    first_num = int(input())
-    second_num = int(input())
-    third_num = int(input())
+def add_and_subtract(num1, num2, num3):
+    sum = sum_numbers(num1, num2)
+    sub = subtract(num3, sum)
+    return sub
 
-    sum = sum_numbers(first_num, second_num)
-    sub = subtract(third_num, sum)
-    print(sub)
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
 
-
-add_and_subtract()
+print(add_and_subtract(first_num, second_num, third_num))
--- python_fundamentals/08_exercise_functions/palindrome_integers.py
+++ python_fundamentals/08_exercise_functions/palindrome_integers.py
@@ -1,14 +1,14 @@
-def palindrome_nums():
-    list_of_nums = input().split(", ")
-    for i in range(len(list_of_nums)):
-        number = list_of_nums[i]
+def palindrome_nums(nums):
+    for i in range(len(nums)):
+        number = nums[i]
         if number == number[::-1]:
             print(True)
         else:
             print(False)
 
 
-palindrome_nums()
+list_of_nums = input().split(", ")
+palindrome_nums(list_of_nums)
 
 
 
