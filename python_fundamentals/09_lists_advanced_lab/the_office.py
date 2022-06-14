c9260e78594300ee2c76377bd8ddebe39a0cb320
1.tsvetanov <ivan.iv.cv@gmail.com>
2022-06-14 12:25:45 +0300

    update the_office

--- python_fundamentals/09_lists_advanced_lab/the_office.py
+++ python_fundamentals/09_lists_advanced_lab/the_office.py
@@ -4,7 +4,7 @@ happiness_factor = int(input())
 multiplied_list = list(map(lambda x: x * happiness_factor, employees_happiness))
 average_res = sum(multiplied_list) / len(multiplied_list)
 
-filtered_list = [x for x in multiplied_list if x > average_res]  # -> filtered_list = list(filter(lambda x: x >= average_res, multiplied_list))
+filtered_list = [x for x in multiplied_list if x >= average_res]  # -> filtered_list = list(filter(lambda x: x >= average_res, multiplied_list))
 
 if len(filtered_list) >= len(multiplied_list) / 2:
     print(f"Score: {len(filtered_list)}/{len(multiplied_list)}. Employees are happy!")
