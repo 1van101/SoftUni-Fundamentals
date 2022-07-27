8c7c60ac36f97232f1ce7cbf43ff3ab2770260d2
IvanTsvetanov <ivan.iv.cv@gmail.com>
2022-07-27 13:35:23 +0300

    add caesar_cipher.py

---
+++ python_fundamentals/16_exercise_text_processing/caesar_cipher.py
@@ -0,0 +1,7 @@
+[print(chr(ord(x) + 3), end="") for x in input()]
+
+# ============================
+# some_text = input()
+# for char in some_text:
+#     encrypted_char = ord(char) + 3
+#     print(chr(encrypted_char), end="")
