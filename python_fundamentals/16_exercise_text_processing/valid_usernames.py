28d70f017daba676a41ac0a80a8532146dc33bc7
IvanTsvetanov <ivan.iv.cv@gmail.com>
2022-07-28 09:35:31 +0300

    replace valid_usernames.py

--- python_fundamentals/16_exercise_text_processing/valid_usernames.py
+++ python_fundamentals/16_exercise_text_processing/valid_usernames.py
@@ -1,19 +1,34 @@
-def lenght(usernames):
-    if 3 <= len(usernames) <= 16:
+def valid_length(name):
+    if 3 <= len(name) <= 16:
         return True
+    return False
 
-def type_of_symbol(usernames):
-    for char in usernames:
-        if not(char.isalnum() or char == "_" or char == "-"):
+
+def valid_content(name):
+    for ch in name:
+        if not ch.isalnum() and not ch == "-" and not ch == "_":
             return False
     return True
 
-def username_validation(usernames):
-    if lenght(usernames) and type_of_symbol(usernames):
+
+def user_is_valid(name):
+    if valid_length(name) and valid_content(name):
         return True
+    return False
 
 
-usernames_input = input().split(", ")
-for username in usernames_input:
-    if username_validation(username):
-        print(username)
+usernames = input().split(", ")
+
+for user in usernames:
+    if user_is_valid(user):
+        print(user)
+
+
+#__________________________________________
+# import re
+#
+# text = input()
+# matches = re.findall(r"(?:(?<=^)|(?<=, ))([\w\-]{3,16})(?:(?=$)|(?=,))", text)
+#
+# [print(x) for x in matches]
+
